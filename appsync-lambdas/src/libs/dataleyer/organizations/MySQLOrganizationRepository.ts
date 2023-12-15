ed7ae87038d6a22afd978d8a0b0e55fd97645a4f
Vitalii Namys <vitaliinamys@MacBook-Pro-Vitalii.local>
2023-12-15 17:54:46 +0200

    SHRPENG-30: Mock DB

--- appsync-lambdas/src/libs/dataleyer/organizations/MySQLOrganizationRepository.ts
+++ appsync-lambdas/src/libs/dataleyer/organizations/MySQLOrganizationRepository.ts
@@ -1,6 +1,65 @@
 import { context, InvocationContext } from '../../middleware/invocation';
 import { MySQLClient } from '../../clients/mysql-client';
 
+const Organizations = [
+  {
+    SOID: '1',
+    Name: 'Organization A',
+    Created: '',
+    Updated: '',
+    Status: 'Active',
+  },
+  {
+    SOID: '2',
+    Name: 'Organization B',
+    Created: '',
+    Updated: '',
+    Status: 'Pending',
+  },
+  {
+    SOID: '3',
+    Name: 'Organization C',
+    Created: '',
+    Updated: '',
+    Status: 'Active',
+  },
+];
+
+const Teams = [
+  {
+    STID: '101',
+    SOID: '1',
+    IntegrationID: 'IntegrationID_101',
+    Name: 'Team X',
+    Created: '',
+    Updated: '',
+    Status: 'Active',
+  },
+  {
+    STID: '102',
+    SOID: '2',
+    IntegrationID: 'IntegrationID_102',
+    Name: 'Team Y',
+    Created: '',
+    Updated: '',
+    Status: 'Pending',
+  },
+  {
+    STID: '103',
+    SOID: '3',
+    IntegrationID: 'IntegrationID_103',
+    Name: 'Team Z',
+    Created: '',
+    Updated: '',
+    Status: 'Active',
+  },
+];
+
+const DB = {
+  Organizations,
+  Teams,
+};
+
 export class MySQLOrganizationRepository {
   private context: InvocationContext;
   private db;
@@ -23,22 +82,40 @@ export class MySQLOrganizationRepository {
     }
   }
 
-  async getSOIDByIntegrationID(integrationID: string) {
-    await this.connect();
+  async getOrganizationByIntegrationID(integrationID: string) {
+   const team = DB.Teams.find((t) => t.IntegrationID === integrationID);
 
-    const [rows] = await this.connection.execute(`
-      SELECT Organizations.SOID
-      FROM Teams
-      JOIN Organizations ON Teams.SOID = Organizations.SOID
-      WHERE Teams.IntegrationID = "${integrationID}"
-    `);
+   if (!team) {
+    return null;
+   }
 
-    this.connection.end();
+   const organization = DB.Organizations.find((o) => o.SOID === team.SOID);
 
-    if (rows.length > 0) {
-      return rows[0].SOID;
-    } else {
-      this.context.logger.error(`Failed to get SOID by ${integrationID}`);
-    }
+   return organization ?? null;
   }
+
+  async getSOIDByIntegrationID(integrationID: string) {
+   const team = DB.Teams.find((t) => t.IntegrationID === integrationID);
+
+   return team?.SOID ?? null;
+  }
+
+  // async getSOIDByIntegrationID(integrationID: string) {
+  //   await this.connect();
+
+  //   const [rows] = await this.connection.execute(`
+  //     SELECT Organizations.SOID
+  //     FROM Teams
+  //     JOIN Organizations ON Teams.SOID = Organizations.SOID
+  //     WHERE Teams.IntegrationID = "${integrationID}"
+  //   `);
+
+  //   this.connection.end();
+
+  //   if (rows.length > 0) {
+  //     return rows[0].SOID;
+  //   } else {
+  //     this.context.logger.error(`Failed to get SOID by ${integrationID}`);
+  //   }
+  // }
 }
